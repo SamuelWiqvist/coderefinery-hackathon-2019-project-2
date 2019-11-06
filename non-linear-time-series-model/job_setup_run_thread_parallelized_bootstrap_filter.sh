71f8c2d44c44c521959f677162e18caf01461ad8
Samuel <swiqvist@gmail.com>
2019-11-06 16:11:42 +0100

    new script for variable

--- non-linear-time-series-model/job_setup_run_thread_parallelized_bootstrap_filter.sh
+++ non-linear-time-series-model/job_setup_run_thread_parallelized_bootstrap_filter.sh
@@ -1,54 +1,58 @@
 #!/bin/bash
 
-nbr_threads=1
-FILE=job_run_thread_parallelized_bootstrap_filter.sh
+nbr_threads1=1
+nbr_threads1=2
+nbr_threads1=3
+nbr_threads1=4
+
+FILE =job_run_thread_parallelized_bootstrap_filter1.sh
 
 cat > $FILE << EOF
-  #!/bin/sh
+#!/bin/sh
 
 
-  # Set up for run:
+# Set up for run:
 
-  # need this since I use a LU project
-  #SBATCH -A lu2019-2-19
-  #SBATCH -p lu
+# need this since I use a LU project
+#SBATCH -A lu2019-2-19
+#SBATCH -p lu
 
-  #SBATCH --qos=test
+#SBATCH --qos=test
 
-  # use gpu nodes
-  #SBATCH -N $nbr_threads
-  #SBATCH --tasks-per-node=10
-  #SBATCH --exclusive
+# use gpu nodes
+#SBATCH -N 1
+#SBATCH --tasks-per-node=$nbr_threads1
+#SBATCH --exclusive
 
-  # time consumption HH:MM:SS
-  #SBATCH -t 00:05:00
+# time consumption HH:MM:SS
+#SBATCH -t 00:05:00
 
-  # name for script
-  #SBATCH -J test_thread_parallel_bootstrap
+# name for script
+#SBATCH -J test_thread_parallel_bootstrap
 
-  # controll job outputs
-  #SBATCH -o lunarc_output/outputs_test_thread_parallel_bootstrap_%j.out
-  #SBATCH -e lunarc_output/errors_test_thread_parallel_bootstrap_%j.err
+# controll job outputs
+#SBATCH -o lunarc_output/outputs_test_thread_parallel_bootstrap_%j.out
+#SBATCH -e lunarc_output/errors_test_thread_parallel_bootstrap_%j.err
 
-  # notification
-  #SBATCH --mail-user=samuel.wiqvist@matstat.lu.se
-  #SBATCH --mail-type=ALL
+# notification
+#SBATCH --mail-user=samuel.wiqvist@matstat.lu.se
+#SBATCH --mail-type=ALL
 
-  # load modules
+# load modules
 
-  ml load GCC/6.4.0-2.28
-  ml load OpenMPI/2.1.2
-  ml load julia/1.0.0
+ml load GCC/6.4.0-2.28
+ml load OpenMPI/2.1.2
+ml load julia/1.0.0
 
-  # set correct path
-  pwd
-  cd ..
-  pwd
+# set correct path
+pwd
+cd ..
+pwd
 
-  export JULIA_NUM_THREADS=$nbr_threads
+export JULIA_NUM_THREADS=$nbr_threads1
 
-  # run program
+# run program
 
-  julia /home/samwiq/'coderefinery-hackathon-2019-project-2'/non-linear-time-series-model/bootstrap_filter_thread_parallelization.jl
+julia /home/samwiq/'coderefinery-hackathon-2019-project-2'/non-linear-time-series-model/bootstrap_filter_thread_parallelization.jl
 
 EOF
