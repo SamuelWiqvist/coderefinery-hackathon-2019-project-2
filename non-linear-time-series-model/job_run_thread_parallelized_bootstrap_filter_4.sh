e71d139494ededbc1415d66529fcedc57e45eb58
Samuel Wiqvist <samwiq@aurora-rviz01.int.lunarc>
2019-11-07 17:08:32 +0100

    run parallelization on lunarc

---
+++ non-linear-time-series-model/job_run_thread_parallelized_bootstrap_filter_4.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# need this since I use a LU project
+#SBATCH -A lu2019-2-19
+#SBATCH -p lu
+
+# for priority
+#SBATCH --qos=test
+
+#SBATCH -N 1
+#SBATCH --tasks-per-node=4
+#SBATCH --exclusive
+
+# time consumption HH:MM:SS
+#SBATCH -t 00:05:00
+
+# name for script
+#SBATCH -J test_thread_parallel_bootstrap
+
+# controll job outputs
+#SBATCH -o lunarc_output/outputs_test_thread_parallel_bootstrap_4_%j.out
+#SBATCH -e lunarc_output/errors_test_thread_parallel_bootstrap_4_%j.err
+
+# notification
+#SBATCH --mail-user=samuel.wiqvist@matstat.lu.se
+#SBATCH --mail-type=ALL
+
+# load modules
+ml load GCC/6.4.0-2.28
+ml load OpenMPI/2.1.2
+ml load julia/1.0.0
+
+# set correct path
+pwd
+cd ..
+pwd
+
+export JULIA_NUM_THREADS=4
+
+# run program
+julia /home/samwiq/'coderefinery-hackathon-2019-project-2'/non-linear-time-series-model/bootstrap_filter_thread_parallelization.jl
